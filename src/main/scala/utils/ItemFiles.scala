7006778a535781ff2be34bc05a3d40215133e073
Vinicius Seixas <vinicius@seixasonline.net>
2023-02-13 02:26:52 +0000

    New version with automatic userId parsing and improved stability and speed

--- src/main/scala/utils/ItemFiles.scala
+++ src/main/scala/utils/ItemFiles.scala
@@ -1,14 +1,13 @@
 package io.pixel
 package utils
 
+import io.pixel.model.*
+import io.pixel.utils.json.*
 import os.Path
 import sttp.model.Uri
 
-import model.*
-import utils.json.*
 
-
-object ItemFiles {
+case class ItemFiles(userId: Long) {
 
 	private val collectionsSubPath = os.sub / "Collections"
 	private val documentsSubPath = os.sub / "Documents"
@@ -17,24 +16,48 @@ object ItemFiles {
 
 	private var rootPath: Option[Path] = None
 
-	def assetExists(asset: Asset): Boolean =
-		os.exists(assetFilename(asset))
+	println(s"=== Collections directory: ${basePath}")
 
-	def hasMetadata(item: Item): Boolean =
-		os.exists(metaFilename(item))
+
+	def assetExists(asset: Asset): Boolean = os.exists(itemAssetFilename(asset))
+
+	def hasMetadata(item: Item): Boolean = os.exists(itemMetaFilename(item))
 
 	def writeMetadata(item: Item): Unit =
-		os.write.over(metaFilename(item), item.definition.toString, createFolders = true)
+		os.write.over(itemMetaFilename(item), item.definition.toString, createFolders = true)
+
 
 	def writeAsset(asset: Asset, data: Array[Byte]): Unit =
-		val filePath = assetFilename(asset)
+		val filePath = itemAssetFilename(asset)
 		os.write.over(filePath, data, createFolders = true)
 
+
 	def loadPieceDetail(piece: Piece): PieceDetail =
-		val metaStr = os.read(metaFilename(piece))
+		val metaStr = os.read(itemMetaFilename(piece))
 		return PieceDetail(piece, JsonObject(metaStr))
 
-	def basePath: Path =
+
+	def writeCollectionMetadata(page: Int, jsonObject: JsonObject): Unit =
+		val collectionMetadata = collectionPageFilename(page)
+		os.write.over(collectionMetadata, jsonObject.toString, createFolders = true)
+
+
+	def loadCollectionPage(page: Int): Option[JsonObject] = {
+		val collectionMetadata = collectionPageFilename(page)
+
+		if !os.exists(collectionMetadata) then
+			return None
+
+		Some(JsonObject(os.read(collectionMetadata)))
+	}
+
+
+	def writeUserMetadata(userName: String, jsonObject: JsonObject): Unit =
+		val userMetadata = userFilename(userName)
+		os.write.over(userMetadata, jsonObject.toString, createFolders = true)
+
+
+	private def basePath: Path =
 		rootPath match
 			case Some(path) => path
 			case None => findBasePath
@@ -48,10 +71,20 @@ object ItemFiles {
 
 		return rootPath.get
 
-	private def metaFilename(item: Item): Path =
+
+	private def userFilename(userName: String) =
+		basePath / "#metadata" / "#users" / s"user_${userId}_$userName.json"
+
+
+	private def collectionPageFilename(page: Int) =
+		basePath / "#metadata" / "#collections" / s"collections_${userId}_$page.json"
+
+
+	private def itemMetaFilename(item: Item): Path =
 		itemPath(item) / metadataSubPath / f"${item.prefix}${item.slug}.json"
 
-	private def assetFilename(asset: Asset): Path =
+
+	private def itemAssetFilename(asset: Asset): Path =
 		val item = asset.item
 		val extension = asset.extension
 		itemPath(item) / imagesSubPath / f"${item.prefix}${item.slug}_${asset.name}.$extension"
